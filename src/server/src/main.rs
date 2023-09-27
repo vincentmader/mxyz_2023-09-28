9d720c15df066f5b942a226b2840d5e18d7a5b56
Vincent C. Mader <vincent.mader@icloud.com>
2023-09-28 01:41:08 +0200

    FEAT Implement serving of HTML files compiled from `.tera` templates

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,12 +1,21 @@
 #[macro_use]
 extern crate rocket;
 
+use rocket_dyn_templates::{context, Template};
+
 #[get("/hello/<name>/<age>")]
 fn hello(name: &str, age: u8) -> String {
     format!("Hello, {} year old named {}!", age, name)
 }
 
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! { msg: "Hello, Tera Templates!" })
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![hello])
+    rocket::build()
+        .attach(Template::fairing())
+        .mount("/", routes![hello, index])
 }
